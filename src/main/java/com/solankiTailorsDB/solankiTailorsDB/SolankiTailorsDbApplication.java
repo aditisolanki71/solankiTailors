6697065e5ea375b27e53c771812228e48c865c00
aditi <adtsolanki22@gmail.com>
2020-05-01 13:02:55 +0530

    initial commit2

---
+++ src/main/java/com/solankiTailorsDB/solankiTailorsDB/SolankiTailorsDbApplication.java
@@ -0,0 +1,13 @@
+package com.solankiTailorsDB.solankiTailorsDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolankiTailorsDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolankiTailorsDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solankiTailorsDB/solankiTailorsDB/SolankiTailorsDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solankiTailorsDB.solankiTailorsDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolankiTailorsDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

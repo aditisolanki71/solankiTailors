7008878472b4c11889999a1f899fa6336d3697ff
Unknown <adtsolanki22@gmail.com>
2020-05-03 20:01:37 +0530

    Item --> getAllItem Operation

--- src/main/java/com/solankiTailorsDB/solankiTailorsDB/Service/UserService.java
+++ src/main/java/com/solankiTailorsDB/solankiTailorsDB/Service/UserService.java
@@ -1,7 +1,10 @@
 package com.solankiTailorsDB.solankiTailorsDB.Service;
 
+import java.util.List;
+
 import com.solankiTailorsDB.solankiTailorsDB.Model.UserModel;
 
 public interface UserService {
 	UserModel addUser(UserModel user);
+	List<UserModel> getAllUser();
 }

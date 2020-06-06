46428ef9613d96c19c62fee5309fed262555ff6e
Unknown <adtsolanki22@gmail.com>
2020-06-07 01:17:53 +0530

    Follow up

--- src/main/java/com/solankiTailorsDB/solankiTailorsDB/Model/FAQModel.java
+++ src/main/java/com/solankiTailorsDB/solankiTailorsDB/Model/FAQModel.java
@@ -16,4 +16,34 @@ public class FAQModel {
 	String question;
 	String answer;
 	String date;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getQuestion() {
+		return question;
+	}
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	@Override
+	public String toString() {
+		return "FAQModel [id=" + id + ", question=" + question + ", answer=" + answer + ", date=" + date + "]";
+	}
+	
+	
 }
